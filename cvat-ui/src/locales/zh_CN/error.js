8bd4874e26b49a0d82109d7c2f418091ab4ebaa7
Ace Code <acecode.tk@gmail.com>
2024-04-29 16:50:41 +0800

    feat:add missing locale file

---
+++ cvat-ui/src/locales/zh_CN/error.js
@@ -0,0 +1,7 @@
+export default {
+    'api-no-info': '无法从服务器获取 {{type}}',
+    'api-not-valid-id': '获取的 {{type}} ID "{{id}}" 不合法',
+    'api-err-unknown': '获取{{type}}过程中出错',
+    'api-ensure-exist-and-access': '请确认资源存在且你有权限',
+    'can-not-update': '无法更新 {{type}}',
+};
